05cd76589149ea4ef0527dc847930d7f5f372472
Pavel <latsh@bk.ru>
2025-03-28 13:47:04 +0300

    Copied: mint story protocol

---
+++ ethers_helper.js
@@ -0,0 +1,184 @@
+const ethers = require('ethers');
+
+/* ========================================================================= */
+// Цветные сообщения
+const HEX = require('./_CONFIGS/chalkColors.json');
+
+const {
+  hexLog, logWarn, logInfo, ceil10, logError, logSuccess, pause, shuffle, 
+  randomBetweenInt, SECOND, consoleTime
+} = require('./helper');
+
+
+/* ========================================================================= */
+function ceilFeeData(feeData, decimals) {
+  // console.log("ceilFeeData", feeData);
+  let newFeeData = {};
+  for (let data in feeData) {
+    if (feeData[data]) {
+      let mutableValue = ethers.formatUnits(feeData[data], 'gwei');
+      // console.log(mutableValue);
+      mutableValue = ceil10(mutableValue, decimals);
+      mutableValue = `${mutableValue}`;
+      // console.log(data, mutableValue);
+      newFeeData[data] = ethers.parseUnits(mutableValue, 'gwei');
+    }
+  }
+  // console.log("feeData ceilFeeData", feeData);
+  return newFeeData;
+};
+
+async function getNetworkGas(BOT, PROJECT_NAME) {
+  let gwei = {};
+  gwei[PROJECT_NAME] = await BOT.providers[PROJECT_NAME].getFeeData()
+    .catch(err => false);
+
+  while (gwei[PROJECT_NAME] === false)   {
+    console.log("Не смогли получить данные о комиссиях");
+    await pause(SECOND * 15);
+    gwei[PROJECT_NAME] = await BOT.providers[PROJECT_NAME].getFeeData()
+      .catch(err => false);
+  }
+
+  gwei[PROJECT_NAME] = ceilFeeData(gwei[PROJECT_NAME], 
+    BOT.configs[PROJECT_NAME].PROJECT_GAS_DECIMALS);
+
+  let gasPrice =  ethers.formatUnits(gwei[PROJECT_NAME].gasPrice, 'gwei');
+  gasPrice = parseFloat(gasPrice);
+
+  // logInfo(`${PROJECT_NAME} | Газ ${gasPrice} | `+
+  // `MAX: ${BOT.configs[PROJECT_NAME].MAX_GWEI_PROJECT}`); 
+
+  BOT.tx_params[PROJECT_NAME].gasPrice = gwei[PROJECT_NAME].gasPrice;
+  return gwei[PROJECT_NAME];
+}
+
+/* ========================================================================= */
+async function waitGwei(BOT, PROJECT_NAME) {
+  // console.log(BOT);
+  // console.log();
+  // console.log("waitGwei | ");
+  process.stdout.write("waitGwei | " + "       " + "\r");
+  let ready = false;
+  let gwei = {};
+  while (!ready) {
+    // logInfo(`Аккаунтов в очереди: ${BOT.queue[PROJECT_NAME]}`);
+    process.stdout.write('\r');  // needs return '/r'
+    await pause(SECOND * 3);
+
+    if (BOT.errors[PROJECT_NAME] > BOT.configs[PROJECT_NAME].MAX_ERRORS) return false;
+
+    try {
+
+      // Если нам нужно следить за Ethereum Gwei
+
+      gwei[PROJECT_NAME] = await BOT.providers[PROJECT_NAME].getFeeData()
+        .catch(err => false);
+      
+      if (!gwei[PROJECT_NAME]) {
+        logError("Не смогли получить данные о комиссиях");
+      }
+      // console.log(gwei)
+
+      gwei[PROJECT_NAME] = ceilFeeData(gwei[PROJECT_NAME], 
+        BOT.configs[PROJECT_NAME].PROJECT_GAS_DECIMALS);
+        
+      let gasPrice =  ethers.formatUnits(gwei[PROJECT_NAME].gasPrice, 'gwei');
+      gasPrice = parseFloat(gasPrice);
+      
+
+      let gasPriceProjectOK = gasPrice < BOT.configs[PROJECT_NAME].MAX_GWEI_PROJECT ;
+
+      // console.log(gwei);
+
+      if (gasPriceProjectOK) {
+        logInfo(`${PROJECT_NAME} | Газ ${gasPrice} | `+
+        `MAX: ${BOT.configs[PROJECT_NAME].MAX_GWEI_PROJECT}`);
+        ready = true;
+        continue;
+      } else {
+        logInfo(`${PROJECT_NAME} | Газ ${gasPrice} | `+
+        `MAX: ${BOT.configs[PROJECT_NAME].MAX_GWEI_PROJECT}`);
+      }
+      // console.log();
+
+    } catch (err) {
+      // console.error(err);
+      logError(`Ошибка при получении газа: ${err.message}`);
+      logWarn(`Ошибок: ${BOT.errors[PROJECT_NAME] + 1} / ${BOT.configs[PROJECT_NAME].MAX_ERRORS}`);
+      BOT.errors[PROJECT_NAME]++;
+      await pause(SECOND * 30);
+    };
+
+    await pause(SECOND * 15);
+  }
+
+
+
+  if (BOT.tx_params[PROJECT_NAME].type === 0) {
+    BOT.tx_params[PROJECT_NAME].gasPrice = gwei[PROJECT_NAME].gasPrice;
+  }
+
+  if (BOT.tx_params[PROJECT_NAME].type === 2) {
+    BOT.tx_params[PROJECT_NAME].gasPrice = null;
+    BOT.tx_params[PROJECT_NAME].maxFeePerGas = gwei[PROJECT_NAME].gasPrice;
+    BOT.tx_params[PROJECT_NAME].maxPriorityFeePerGas = gwei[PROJECT_NAME].gasPrice;
+  } 
+
+  // console.log(BOT.configs[PROJECT_NAME]);
+  // console.log(BOT.tx_params[PROJECT_NAME]);
+
+  return gwei[PROJECT_NAME];
+
+};
+
+/* ========================================================================= */
+// Функция для увеличения количества газа
+function gasMultiplicate(amount, multiplicate) {
+  let amountFloat = ethers.formatUnits(amount, `wei`);
+  amountFloat = amountFloat * multiplicate;
+  return ethers.parseUnits(`${parseInt(Math.ceil(amountFloat))}`, `wei`);
+}
+
+/* ========================================================================= */
+// Функция для пауз между транзакциями
+async function pauseBetweenTx({min, max}) {
+  if (!min || !max) {
+    logWarn(`В функцию pauseBetweenTx не переданы значения для min или max`);
+    return false;
+  }
+  let pauseInMs = randomBetweenInt(min, max);
+  pauseInMs = pauseInMs * 1000;
+  logInfo(`Пауза между активностями ${parseInt(pauseInMs / 1000)} секунд`);
+
+  return await pause(pauseInMs);
+}
+
+/* ========================================================================= */
+// Получаем кошелек
+async function getWallet(privateKey, provider) {
+  // console.log(privateKey, provider)
+  let wallet = new ethers.Wallet(privateKey, provider);
+  // console.log(wallet.address);
+  return wallet;
+};
+
+async function getBalance(BOT, PROJECT_NAME) {
+  // console.log("getBalance:")
+  try {
+    return await BOT.providers[PROJECT_NAME]
+      .getBalance(BOT.wallets[PROJECT_NAME].address);
+  } catch (error) {
+  logError(`${PROJECT_NAME} | ${BOT.session} | Не смогли получить баланс | `
+      + error.message);
+    return false;
+  }
+}
+
+/* ========================================================================= */
+exports.waitGwei = waitGwei;
+exports.getNetworkGas = getNetworkGas;
+exports.gasMultiplicate = gasMultiplicate;
+exports.pauseBetweenTx = pauseBetweenTx;
+exports.getWallet = getWallet;
+exports.getBalance = getBalance;
---
+++ index.js
@@ -0,0 +1,336 @@
+/* ========================================================================= */
+// Библиотеки
+const fs = require('fs');
+const inquirer = require('inquirer');
+const ethers = require('ethers')
+/* ========================================================================= */
+// Модули
+const mint = require('./mint').mint;
+const {logError, logInfo, hexLog, logWarn, evaluateProxy, logSuccess, pause,
+  randomBetweenInt, consoleTime,
+  MINUTE, SECOND,
+  shuffle} = require('./helper');
+const {getWallet, getBalance} = require('./ethers_helper');
+const {createProvider} = require('./providers');
+// console.log(mint);
+
+/* ========================================================================= */
+// Код
+const mintArray = Object.keys(mint).map(element => element).reverse();
+
+/* ========================================================================= */
+// Меню
+const questions = [
+  {
+    name: "choice",
+    type: "list",
+    message: " ",
+    choices: mintArray.map(function(key){
+      return {
+        name: (!mint[key].ended) ? mint[key].name : `${mint[key].name} [ENDED]`, 
+        value: key}
+    })
+  }
+];
+
+/* ========================================================================= */
+  async function Main () {
+    // Выбор минта
+    const answers = await inquirer.prompt(questions);
+    let choice = await answers.choice;
+    // let choice = 2;
+    // console.log("Выбрали для минта:", mint[choice].name, choice);
+    logInfo(`Выбрали для минта: ${hexLog(mint[choice].name, 'balance')}`)
+
+    if (mint[choice].ended || choice === "mintFunctions") {
+      logError(`Выбрали неактивный минт.`);
+      return false;
+    }
+
+    // Проверяем файл unready.txt
+    const unreadyExist = fs.existsSync('./_CONFIGS/unready.txt');
+    if (!unreadyExist) {
+      logError(`Файл unready.txt не существует. Создаю`);
+      fs.writeFileSync(`./_CONFIGS/unready.txt`, ``, `utf-8`);
+
+      // Проверяем приватные адреса
+      const privatesExist = fs.existsSync('privates.txt');
+      if (!privatesExist) {
+        logError(`Файл privates.txt не существует. Создаю`);
+        fs.writeFileSync(`privates.txt`, ``, `utf-8`);
+        return false;
+      }
+
+      // Считываем приватные адреса
+      const privateKeys = fs.readFileSync(`privates.txt`, `utf-8`)
+        .split("\n")
+        .map(row => row.trim())
+        .filter(pk => pk !== "");
+      // console.log(privateKeys.length, privateKeys);
+      if (privateKeys.length === 0) {
+        logError(`Файл privates.txt пуст.`);
+        return false;
+      }
+
+      // Проверяем прокси
+      const proxyExist = fs.existsSync('proxy.txt');
+      if (!proxyExist) {
+        logError(`Файл proxy.txt не существует. Создаю`);
+        fs.writeFileSync(`proxy.txt`, ``, `utf-8`);
+        return false;
+      }
+
+      // Считываем прокси
+      const proxyList = fs.readFileSync(`proxy.txt`, `utf-8`)
+        .split("\n")
+        .map(row => row.trim())
+        .filter(proxy => proxy !== "");
+      if (proxyList.length === 0) {
+        logError(`Файл proxy.txt пуст.`);
+        return false;
+      }
+      
+      // Проверяем соответствие
+      if (proxyList.length !== privateKeys.length) {
+        logError(`Количество прокси не соответствует количеству приватных ключей`);
+        logWarn(`Прокси: ${proxyList.length}. Приватных ключей: ${privateKeys.length}`)
+        return false;
+      }
+
+      // Объединяем privateKeys и proxyList
+      let unready = ``;
+      for await (let [i, privateKey] of privateKeys.entries()) {
+        unready += `id${i+1};${privateKey};${proxyList[i]}\n`;
+      }
+      fs.writeFileSync(`./_CONFIGS/unready.txt`, unready, `utf-8`);
+
+      return false;
+    }
+
+    const configExist = fs.existsSync('./_CONFIGS/STORY.json');
+    if (!configExist) {
+      logError(`Конфиг STORY.json не существует. Копирую конфиг по умолчанию.`);
+      logWarn(`Заполните STORY.json`);
+      fs.copyFileSync('./_CONFIGS/STORY.json_', './_CONFIGS/STORY.json');
+      return false;
+    };
+
+    const CONFIG = require('./_CONFIGS/STORY.json');
+    // console.log(CONFIG);
+    if (CONFIG.SHUFFLE_PK) {
+      logWarn(`Включена рандомизация кошельков`);
+    };
+
+    // Стартуем
+    // const privateKeysOld = JSON.parse(JSON.stringify(privateKeys));
+    let unready = fs
+      .readFileSync(`./_CONFIGS/unready.txt`, `utf-8`)
+      .split("\n")
+      .filter(row => row !== "");
+
+    unready =  (CONFIG.SHUFFLE_PK === true) ? shuffle(unready) : unready;
+
+    logWarn(`RPC: ${CONFIG.RPC}`);
+    if (CONFIG.MAX_GWEI_ETHEREUM) logWarn(`Включено отслеживание gwei в Ethereum: ${CONFIG.MAX_GWEI_ETHEREUM}`);
+    if (!CONFIG.MAX_ERRORS) {
+      logError(`Не установлен параметр MAX_ERRORS в конфиге`);
+      return;
+    }
+
+    if (!CONFIG.hasOwnProperty(`TXN_TYPE`)) {
+      logError(`Не установлен параметр TXN_TYPE в конфиге`);
+      return; 
+    }
+
+    logWarn(`Количество ошибок: ${CONFIG.MAX_ERRORS}`);
+    logWarn(`Максимальный gwei: ${CONFIG.MAX_GWEI_PROJECT}`);
+    logWarn(`Пауза между кошельками: от ${CONFIG.PAUSE_BETWEEN_ACCOUNTS[0]} до ${CONFIG.PAUSE_BETWEEN_ACCOUNTS[1]} секунд`);
+    logWarn(`Тип прокси: ${CONFIG.PROXY_TYPE}`);
+    logWarn(`Тип транзакции: ${CONFIG.TXN_TYPE}`);
+    logWarn(`Рандомный тип транзакции: ${CONFIG.RANDOM_TXN_TYPE}`);
+    logWarn(`Ожидание транзакции: ${CONFIG.WAIT_TX}`);
+    logWarn(`RPC: ${CONFIG.RPC}`);
+    console.log();
+
+    if (unready.length === 0) {
+      logError(`Файл unready.txt существует, но пуст.`);
+      fs.unlinkSync(`./_CONFIGS/unready.txt`);
+      return false;
+    }
+
+    let length = unready.length;
+    fs.appendFileSync(`./_LOGS/logs.txt`, `${"*".repeat(100)}\n${consoleTime()}\n${mint[choice].name} | Кошельков: ${length}\n${"*".repeat(100)}\n`);
+
+    for await (let [i, row] of unready.entries()) {
+
+      try {
+      // console.log(row);
+      console.log();
+
+      let [id, privateKey, proxy] = row.split(";").map(el => el.trim());
+      // console.log(id, proxy);
+      // continue;
+
+      if (!proxy || !privateKey) continue;
+
+      const BOT = {};
+      await createBot(BOT, privateKey, proxy);
+      // console.log(BOT);
+
+      let balance = await getBalance(BOT, "STORY");
+
+      if (balance === false) {
+        delete BOT;
+        continue;
+      } 
+
+      balance = ethers.formatEther(balance);
+      logInfo(BOT.wallets["STORY"].address)
+
+
+      let standardMsg = `Кошелек [${BOT.wallets["STORY"].address} | ${id}] [${parseInt(i) + 1} из ${length}]`;
+      standardMsg += ` | ${mint[choice].name} `;
+
+      logInfo(standardMsg);
+
+      logInfo(`Баланс кошелька: ${balance}`);
+
+      if (balance < CONFIG.MIN_BALANCE) {
+        logWarn(`Слишком маленький баланс`);
+        fs.appendFileSync(`./_LOGS/res.txt`, `${BOT.wallets["STORY"].address}\tFALSE\t${balance}\n`, `utf-8`);
+        delete BOT;
+        continue;
+      };
+
+        //Ждем газ и выставляем параметры транзакции (gasPrice)
+        // BOT.tx_params["STORY"].maxFeePerGas = 0;
+        // BOT.tx_params["STORY"].maxPriorityFeePerGas = 0;
+
+        // Txn Type: 0 (Legacy) Rabby Wallet ???
+        BOT.tx_params["STORY"].type = CONFIG.TXN_TYPE;
+        
+        if (CONFIG.RANDOM_TXN_TYPE) {
+          BOT.tx_params["STORY"].type = shuffle([0,2])[0];
+        }
+
+        // console.log(BOT.tx_params["STORY"]);
+
+        let msg = ``;
+
+        // Делаем минт
+        let tx = await  mint.mintFunctions[choice](BOT, mint[choice]);
+
+        if (tx === true) {
+          logSuccess(standardMsg + `| Минт уже был совершен.`);
+          msg = consoleTime() + " | " + standardMsg + `| Минт уже был совершен.\n`;
+        }
+        else {
+
+          if (tx === false) {
+            logError(standardMsg + `| Не смогли заминтить`);
+            msg += consoleTime() + " | " + standardMsg + `| Не смогли заминтить\n`
+          } else {
+            logWarn(standardMsg + `| | Type: ${BOT.tx_params["STORY"].type} | ${tx.hash}`);
+            msg = consoleTime() + " | " + standardMsg + `| Транзакция в очереди | Type: ${BOT.tx_params["STORY"].type} | ${CONFIG.EXPLORER}/${tx.hash}\n`;
+            if (CONFIG.WAIT_TX) {
+              await tx.wait();
+              logSuccess(standardMsg + `| ${tx.hash}`);
+              msg += consoleTime() + " | " + standardMsg + `| Транзакция готова | ${CONFIG.EXPLORER}/${tx.hash}\n`;  
+            }
+          }
+
+        }
+
+        // Записываем логи и обновляем файлы
+        fs.appendFileSync(`./_LOGS/logs.txt`, msg, `utf-8`);
+        fs.appendFileSync(`./_CONFIGS/ready.txt`, row + "\n", `utf-8`);
+
+        // Удаляем кошелек из файла неготовых
+        unready = unready.filter(el => el !== row); 
+        fs.writeFileSync(`./_CONFIGS/unready.txt`, unready.join("\n"), `utf-8`);
+       
+        // Если минт был совершен ранее, то пропускаем паузу
+        if (tx === true) {
+          fs.appendFileSync(`./_LOGS/res.txt`, `${BOT.wallets["STORY"].address}\tTRUE\t${balance}\n`, `utf-8`);
+          delete BOT;
+          continue;
+        };
+
+        if (tx === false) {
+          fs.appendFileSync(`./_LOGS/res.txt`, `${BOT.wallets["STORY"].address}\tFALSE\t${balance}\n`, `utf-8`);
+          delete BOT;
+          continue;
+        }
+
+        // Если последний кошелек, то ждать не нужно
+        // console.log(i, i+1, i+1 === unready.length, unready.length);
+        if (i+1 === length) {
+          delete BOT;
+          continue;
+        }
+
+        // Пауза между кошельками
+        let pauseSeconds = randomBetweenInt(
+          CONFIG.PAUSE_BETWEEN_ACCOUNTS[0],
+          CONFIG.PAUSE_BETWEEN_ACCOUNTS[1]
+        );
+        let pauseSecondsMs = pauseSeconds * SECOND;
+        logInfo(standardMsg + ` | Пауза ${pauseSecondsMs / SECOND} секунд`);
+
+        // Пауза между кошельками
+        await pause(pauseSecondsMs);
+        
+      } catch (err) {
+        logError(err.message);
+        // logError(standardMsg + ` | ${err.message}`);
+        // msg = consoleTime() + " | " + standardMsg + ` | ERROR: ${err.message}` + "\n";
+        // fs.appendFileSync(`./_LOGS/logs.txt`, msg, `utf-8`);
+        // fs.appendFileSync(`./_CONFIGS/fail.txt`, row + "\n", `utf-8`);
+        console.error(err);
+      }
+
+    };
+
+
+
+    // createBot
+    async function createBot(BOT, privateKey, proxy) {
+      BOT.configs = {"STORY": CONFIG};
+      BOT.tx_params = {"STORY": {}};
+      // Создаем провайдера
+      BOT.providers = {};
+
+      BOT.proxy = evaluateProxy(proxy, CONFIG.PROXY_TYPE);
+      // console.log(BOT.proxy);
+      // return;
+
+      BOT.providers["STORY"] = await createProvider({
+        RPC: CONFIG.RPC,
+        proxy: BOT.proxy
+      });
+
+      // Создаем кошелек
+      BOT.wallets = {};
+      BOT.wallets["STORY"] = await getWallet(privateKey, BOT.providers["STORY"]);
+      BOT.errors = {};
+      BOT.errors["STORY"] = 0;
+
+      return BOT;
+    }
+
+    return true;
+}
+
+(async () => {
+
+  while (true) {
+    let res = await Main();
+    logSuccess(`ЗАВЕРШИЛИ ЦИКЛ`);
+    console.log();
+    console.log();
+    console.log();
+    await pause( 10 * SECOND);
+    if (res === true) await pause( 10 * MINUTE);
+  }
+
+})();
