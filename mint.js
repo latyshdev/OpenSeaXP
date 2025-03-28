48e31fa7f5742ce163f061976ae32a9eff3676a7
Pavel <latsh@bk.ru>
2025-03-28 16:22:54 +0300

    try catch mint

--- mint.js
+++ mint.js
@@ -1,6 +1,6 @@
 /* ========================================================================= */
 const ethers = require('ethers');
-const {gasMultiplicate, waitGwei} = require('./ethers_helper');
+const { gasMultiplicate, waitGwei } = require('./ethers_helper');
 const { logError, pause, SECOND, logInfo, logSuccess } = require('./helper');
 /* ========================================================================= */
 exports.mint = {
@@ -17,7 +17,7 @@ exports.mint = {
     feeRecipient: `0x0000a26b00c1F0DF003000390027140000fAa719`,
     value: `0`,
     ended: false
-  }, 
+  },
 
   2: {
     name: `Jin on a Sailing Ship [Price: 0 RON]`,
@@ -40,42 +40,49 @@ exports.mint = {
 
 /* ========================================================================= */
 async function openSeaPublicMint(BOT, choice) {
-  const ABI = require(`./OpenEditionERC721.json`); 
-  const contractNFT = new ethers.Contract(choice.NFT, ABI, BOT.wallets["RONIN"]);
 
-  const mintABI = `["function mintPublic(address,address,address,uint256)"]`;
-  const mintContract = new ethers.Contract(choice.mint, mintABI, BOT.wallets["RONIN"]);
+  try {
 
-  const balanceOf = await contractNFT.balanceOf(BOT.wallets["RONIN"].address);
-  // console.log("balanceOf", balanceOf);
-  if (balanceOf > 0) {
-    return true;
-  } else {
-    // Ждем газ
-    let gasIsNormal = await waitGwei(BOT, "RONIN");
-    if (!gasIsNormal) return false;
+    const ABI = require(`./OpenEditionERC721.json`);
+    const contractNFT = new ethers.Contract(choice.NFT, ABI, BOT.wallets["RONIN"]);
+
+    const mintABI = `["function mintPublic(address,address,address,uint256)"]`;
+    const mintContract = new ethers.Contract(choice.mint, mintABI, BOT.wallets["RONIN"]);
+
+    const balanceOf = await contractNFT.balanceOf(BOT.wallets["RONIN"].address);
+    // console.log("balanceOf", balanceOf);
+    if (balanceOf > 0) {
+      return true;
+    } else {
+      // Ждем газ
+      let gasIsNormal = await waitGwei(BOT, "RONIN");
+      if (!gasIsNormal) return false;
 
 
-    // Определяем сколько нужно газа на транзакцию
-    const gasAmount = await mintContract["mintPublic"]
-      .estimateGas(
+      // Определяем сколько нужно газа на транзакцию
+      const gasAmount = await mintContract["mintPublic"]
+        .estimateGas(
+          choice.NFT,
+          choice.feeRecipient,
+          BOT.wallets["RONIN"].address,
+          1,
+          BOT.tx_params["RONIN"]
+        );
+
+      BOT.tx_params["RONIN"].gasLimit = gasMultiplicate(gasAmount, BOT.configs["RONIN"].GAS_AMOUNT_MULTIPLICATOR);
+      // console.log(gasAmount, BOT.tx_params["RONIN"].gasLimit);
+
+      let tx = await mintContract["mintPublic"](
         choice.NFT,
         choice.feeRecipient,
         BOT.wallets["RONIN"].address,
-        1,   
+        1,
         BOT.tx_params["RONIN"]
       );
-    
-    BOT.tx_params["RONIN"].gasLimit = gasMultiplicate(gasAmount, BOT.configs["RONIN"].GAS_AMOUNT_MULTIPLICATOR);
-    // console.log(gasAmount, BOT.tx_params["RONIN"].gasLimit);
-
-    let tx = await mintContract["mintPublic"](
-      choice.NFT,
-      choice.feeRecipient,
-      BOT.wallets["RONIN"].address,
-      1,   
-      BOT.tx_params["RONIN"]
-    );
-    return tx; 
+      return tx;
+    }
+  } catch (error) {
+    logError(`Ошибка при минте: ${error.message}`);
+    return false;
   }
-}
+}   
